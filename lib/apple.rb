86ecaf7cbfff47469e18491c88e56c672ad393a2
NancyKo <konancys@gmail.com>
2015-08-03 13:36:33 -0400

    Finished 1-5

--- lib/apple.rb
+++ lib/apple.rb
@@ -8,7 +8,13 @@ def apple_picker_with_collect(fruit_list)
   # fruit_list will look something like: ["apple", "banana", "apple"]
   # you want to return an array that just the apples, just like the 
   # challenge above
+  # 
+  
+  # Collect is already going to give us a new array, it just needs the instructions of 
+  # what to put into the new array. Instructions are found in the code block {} 
+  # Code block says if an item is equal to "apple" give me the item. 
   result_of_collecting = fruit_list.collect {|item| item if item == "apple" } 
+  
   result_of_collecting.compact
 
 end
--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,48 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+    # holiday_hash.keys
+    holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above 
   # add the second argument, which is a supply, to BOTH the 
   # Christmas AND the New Year's arrays
-  
+  # holiday_hash[:winter][:christmas] << "Balloons"
+  # holiday_hash[:winter][:new_years] << "Balloons"
+
+  season = holiday_hash[:winter]
+  season.each do |k, v| 
+    v << supply
+  end 
 end
  
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-  
+  season = holiday_hash[:spring]
+  season.each do |k, v|
+    v << supply
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  
+  holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  
+  winter_supplies = []
+  holiday_hash[:winter].each do |k, v|
+    v.each do |supply|
+      winter_supplies << supply
+    end
+  end
+  return winter_supplies
 end
 
 def all_supplies_in_holidays(holiday_hash)
